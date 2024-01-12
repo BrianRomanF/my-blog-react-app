af202ef492954e186aa0d6d6d4675d1e54ed983f
BrianRomanF <broman.89@gmail.com>
2024-01-13 12:29:38 +1300

    second commit

--- src/server.js
+++ src/server.js
@@ -9,12 +9,12 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const credentials = JSON.parse(
+/* const credentials = JSON.parse(
     fs.readFileSync('./credentials.json')
 );
 admin.initializeApp({
     credential: admin.credential.cert(credentials),
-});
+}); */
 
 const app = express();
 app.use(express.json());
