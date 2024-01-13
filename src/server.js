5f402c7ff4d6351b347b1e1a14fb82f891beb92f
BrianRomanF <broman.89@gmail.com>
2024-01-13 13:49:52 +1300

    changes

--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const credentials = JSON.parse(fs.readFileSync('./etc/secrets/credentials.json' ));
+const credentials = JSON.parse(fs.readFileSync('/etc/secrets/credentials.json' ));
 
 
 admin.initializeApp({ credential: admin.credential.cert(credentials) });
